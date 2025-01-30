58a2a3f0761004b4da3d4bf04a701c9aa2f75760
alihv <alihv5000@gmail.com>
2025-01-30 16:10:47 +0330

    fix: fix unfold inlines bug in persain language

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_me(filename):
 
 setup(
     name='ez-django-common',
-    version='1.0.4',
+    version='1.0.5',
     python_requires='>=3',
     packages=find_packages(),
     include_package_data=True,
