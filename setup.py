c66e92ac881ca0ccb3af7ac2fd55d1def62f2a33
alihv <alihv5000@gmail.com>
2025-03-09 10:47:30 +0330

    chore: add missing translation

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_me(filename):
 
 setup(
     name='ez-django-common',
-    version='1.0.14',
+    version='1.0.15',
     python_requires='>=3',
     packages=find_packages(),
     include_package_data=True,
