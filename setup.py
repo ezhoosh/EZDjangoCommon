e38c0bc156d8327962a1ae2c9a93c37986975dc1
alihv <alihv5000@gmail.com>
2025-06-12 13:06:52 +0330

    feat: add translation for django boolean field display

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_me(filename):
 
 setup(
     name='ez-django-common',
-    version='1.0.15',
+    version='1.0.16',
     python_requires='>=3',
     packages=find_packages(),
     include_package_data=True,
